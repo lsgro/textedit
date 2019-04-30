254f9e1c932fdb58d1d5dc027bfa60dc65ece337
LSgro <LSgro@argentiere.ch>
2019-04-30 13:33:15 +0200

    Added arrow movement

--- terminal.js
+++ terminal.js
@@ -21,22 +21,26 @@ function joinRows(total, currentValue) {
 }
 
 function redisplay() {
+    console.log(buffer, "row: ", pointRow, "col: ", pointCol);
     var html = buffer.reduce(joinRows);
     viewport.innerHTML = html;
 }
 
 function newLine() {
     if (pointCol == 0) {
-        buffer = buffer.splice(pointRow, 0, "");
+        buffer.splice(pointRow, 0, "");
     } else {
         var line = buffer[pointRow],
-            segments;
+            head,
+	    tail;
         if (pointCol == line.length) {
-            segments = [line, ""];
+	    head = line;
+	    tail = "";
         } else {
-            segments = line.split(pointCol);
+	    head = line.substring(0, pointCol);
+	    tail = line.substring(pointCol);
         }
-        buffer = buffer.splice(pointRow, 1, segments[0], segments[1]);
+        buffer.splice(pointRow, 1, head, tail);
     }
     pointRow += 1;
     pointCol = 0;
@@ -64,9 +68,53 @@ function removeCharLeft() {
         if (pointRow > 0) {
             buffer.splice(pointRow, 1);
             pointRow -= 1;
+	    pointCol = buffer[pointRow].length;
             removeCharLeft();
         }
     }
+}
+
+function removeCharRight() {
+    var line = buffer[pointRow];
+    if (pointCol < line.length) {
+	var nextLine = line.substring(0, pointCol).concat(line.substring(pointCol + 1));
+	buffer[pointRow] = nextLine;
+    } else {
+	// TODO make it configurable
+	removeCharLeft();
+    }
+}
+
+function moveLeft() {
+    if (pointCol > 0) {
+	pointCol -= 1;
+    } else if (pointRow > 0) {
+	pointRow -= 1;
+	pointCol = buffer[pointRow].length;
+    }
+}
+
+function moveRight() {
+    if (pointCol < buffer[pointRow].length) {
+	pointCol += 1;
+    } else if (pointRow < buffer.length - 1) {
+	pointRow += 1;
+	pointCol = 0;
+    }
+}
+
+function moveUp() {
+    if (pointRow > 0) {
+	pointRow -= 1;
+	pointCol = Math.min(pointCol, buffer[pointRow].length);
+    }
+}
+
+function moveDown() {
+    if (pointRow < buffer.length - 1) {
+	pointRow += 1;
+	pointCol = Math.min(pointCol, buffer[pointRow].length);
+    }	
 }
 
 function handleKeys(e) {
@@ -77,12 +125,27 @@ function handleKeys(e) {
     case "Backspace":
         removeCharLeft();
         break;
+    case "Delete":
+	removeCharRight();
+	break;
     case "Enter":
         newLine();
         break;
     case "Tab":
         addChar(tab);
         break;
+    case "ArrowLeft":
+	moveLeft();
+	break;
+    case "ArrowRight":
+	moveRight();
+	break;
+    case "ArrowUp":
+	moveUp();
+	break;
+    case "ArrowDown":
+	moveDown();
+	break;
     default:
         addChar(e.key);
     }
